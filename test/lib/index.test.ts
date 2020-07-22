7c1253f201101e091b437d037a7e2fc063d63e3f
aarlaud <antoine@snyk.io>
2020-07-22 14:50:17 +0200

    chore: ignore ESLINT rule for gh POST data struct

--- test/lib/index.test.ts
+++ test/lib/index.test.ts
@@ -1,9 +1,9 @@
 import * as nock from 'nock';
 import * as fs from 'fs';
-import {main} from '../../src/lib/index'
-import { request } from 'http';
+import { main } from '../../src/lib/index';
 
-const fixturesFolderPath = '/home/antoine/Documents/SnykTSDev/snyk-prevent-gh-commit-status/test/fixtures/';
+const fixturesFolderPath =
+  '/home/antoine/Documents/SnykTSDev/snyk-prevent-gh-commit-status/test/fixtures/';
 
 beforeAll(() => {
   return nock('https://snyk.io')
@@ -16,9 +16,7 @@ beforeAll(() => {
             fixturesFolderPath + 'api-response-projects.json',
           );
         case '/api/v1/org/Playground/project/09235fa4-c241-42c6-8c63-c053bd272786/issues':
-          return fs.readFileSync(
-            fixturesFolderPath + 'apitest-gomod.json',
-          );
+          return fs.readFileSync(fixturesFolderPath + 'apitest-gomod.json');
         default:
       }
     });
@@ -29,37 +27,56 @@ beforeAll(() => {
     .persist()
     .post(/^(?!.*xyz).*$/)
     .reply(200, (uri, requestBody) => {
-      switch(uri){
+      switch (uri) {
         case '/repos/123/123/statuses/123':
-          return requestBody
+          return requestBody;
         default:
-          throw new Error('unexpected status POSTing to Github')
+          throw new Error('unexpected status POSTing to Github');
       }
     });
 });
 
-
 describe('Testing behaviors', () => {
   test('Is it working?', async () => {
-    process.argv = ['','','/home/antoine/Documents/SnykTSDev/snyk-prevent-gh-commit-status/test/fixtures/snyktest-gomod.json','123','123','123','123']
-    const response = await main()
+    process.argv = [
+      '',
+      '',
+      '/home/antoine/Documents/SnykTSDev/snyk-prevent-gh-commit-status/test/fixtures/snyktest-gomod.json',
+      '123',
+      '123',
+      '123',
+      '123',
+    ];
+    const response = await main();
     expect(response).toEqual({
-                              "context": "Snyk Prevent (Playground)",
-                             "description": "No new issue found", 
-                             "state": "success", 
-                             "target_url": "https://app.snyk.io/org/Playground/project/09235fa4-c241-42c6-8c63-c053bd272786"
-    })
+      context: 'Snyk Prevent (Playground)',
+      description: 'No new issue found',
+      state: 'success',
+      // eslint-disable-next-line
+      target_url:
+        'https://app.snyk.io/org/Playground/project/09235fa4-c241-42c6-8c63-c053bd272786',
+    });
   });
-  
-  test('Is it working in debug?', async () => {
-    process.argv = ['','','/home/antoine/Documents/SnykTSDev/snyk-prevent-gh-commit-status/test/fixtures/snyktest-gomod.json','123','123','123','123','debug']
-    const response = await main()
-    expect(response).toEqual({
-                              "context": "Snyk Prevent (Playground)",
-                             "description": "No new issue found", 
-                             "state": "success", 
-                             "target_url": "https://app.snyk.io/org/Playground/project/09235fa4-c241-42c6-8c63-c053bd272786"
-    })
-  });
-})
 
+  test('Is it working in debug?', async () => {
+    process.argv = [
+      '',
+      '',
+      '/home/antoine/Documents/SnykTSDev/snyk-prevent-gh-commit-status/test/fixtures/snyktest-gomod.json',
+      '123',
+      '123',
+      '123',
+      '123',
+      'debug',
+    ];
+    const response = await main();
+    expect(response).toEqual({
+      context: 'Snyk Prevent (Playground)',
+      description: 'No new issue found',
+      state: 'success',
+      // eslint-disable-next-line
+      target_url:
+        'https://app.snyk.io/org/Playground/project/09235fa4-c241-42c6-8c63-c053bd272786',
+    });
+  });
+});
